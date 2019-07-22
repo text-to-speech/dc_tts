9f31df7bb44bf67d7223964dd0ad247e8365c59b
Leggera <lana.krlv1995@gmail.com>
2019-07-22 22:53:09 +0300

    Update prepro.py

--- prepro.py
+++ prepro.py
@@ -12,14 +12,16 @@ import os
 from data_load import load_data
 import numpy as np
 import tqdm
+from hyperparams import Hyperparams as hp
 
 # Load data
 fpaths, _, _ = load_data() # list
 
 for fpath in tqdm.tqdm(fpaths):
     fname, mel, mag = load_spectrograms(fpath)
-    if not os.path.exists("mels"): os.mkdir("mels")
-    if not os.path.exists("mags"): os.mkdir("mags")
+    
+    if not os.path.exists(hp.data+"mels"): os.mkdir(hp.data+"mels")
+    if not os.path.exists(hp.data+"mags"): os.mkdir(hp.data+"mags")
 
-    np.save("mels/{}".format(fname.replace("wav", "npy")), mel)
-    np.save("mags/{}".format(fname.replace("wav", "npy")), mag)
+    np.save(hp.data+"mels/{}".format(fname.replace("wav", "npy")), mel)
+    np.save(hp.data+"mags/{}".format(fname.replace("wav", "npy")), mag)
