26544128af14722e2e78b29463260c5965ce6e70
Leggera <lana.krlv1995@gmail.com>
2019-07-23 14:22:38 +0300

    Update synthesize.py

--- synthesize.py
+++ synthesize.py
@@ -31,13 +31,13 @@ def synthesize():
         # Restore parameters
         var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'Text2Mel')
         saver1 = tf.train.Saver(var_list=var_list)
-        saver1.restore(sess, tf.train.latest_checkpoint(hp.logdir + "-1"))
+        saver1.restore(sess, tf.train.latest_checkpoint(hp.logdir + "1"))
         print("Text2Mel Restored!")
 
         var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'SSRN') + \
                    tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 'gs')
         saver2 = tf.train.Saver(var_list=var_list)
-        saver2.restore(sess, tf.train.latest_checkpoint(hp.logdir + "-2"))
+        saver2.restore(sess, tf.train.latest_checkpoint(hp.logdir + "2"))
         print("SSRN Restored!")
 
         # Feed Forward
